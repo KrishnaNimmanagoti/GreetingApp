0aa978ab58ba48120d8603c7c11870af6fd3697a
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-25 23:51:03 +0530

    Refactored Greeting App to give message wit firs and last name

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +14,22 @@ import com.example.demo.model.Greeting;
 import com.example.demo.service.IGreetingService;
 
 @RestController
+@RequestMapping({"/", " "})
 public class GreetingController {
 	
 	@Autowired
 	private IGreetingService greetingService;
 	
 	@GetMapping("/getGreeting")
-	public Greeting getGreeting(@RequestParam(value="name", defaultValue="World") String name) {
+	public Greeting getGreeting(@RequestParam(value="firstName") Optional<String> firstName, 
+			@RequestParam(value="lastName") Optional<String> lastName) {
+		String name = "";
+		if(firstName.isPresent())
+			name += firstName.get();
+		if(lastName.isPresent())
+			name += " " + lastName.get();
+		if(name.length() == 0)
+			name = "Hello World";
 		return greetingService.getGreeting(name);
 	}
 	
